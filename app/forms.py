e818d6ddf57a957097e634dad65cf16957ba7836
Sajid Kamadia <sarkamadia@mun.ca>
2018-03-08 18:49:41 -0330

    Made changes to login controller

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-	aUsername = StringField("Username", validators=[DataRequired()])
-	aPassword = PasswordField("Password", validators=[DataRequired()])
-	aRemeber_Me = BooleanField("Remember Me?")
-	aSubmit = SubmitField("Log in")
