a59857060b828ee7a2f62c159960b82a0be2b05b
mingrobb <33940684+mingrobb@users.noreply.github.com>
2018-03-14 18:48:55 -0230

    Delete PostController.py

--- app/controllers/PostController.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, flash, redirect, url_for, request, session
-from app.models.Post import Post
-
-class PostController:
-
-	@staticmethod
-	def get():
-        return render_template("Post.html")
