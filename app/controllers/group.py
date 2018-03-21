8e3253d59b0fd6b30918e83509affd3d6439eedd
Moustafa Elsisy <spaceflyby@gmail.com>
2018-03-21 19:20:45 -0230

    * Added the create discussion group view

--- app/controllers/group.py
+++ app/controllers/group.py
@@ -1,6 +1,9 @@
 from flask import render_template, session, redirect, request
-from app.models.Group import Group
+# from app.models.Group import Group
+from app.models.User import User
+from app.services.JsonService import JsonService
 from app import db
+import json
 
 
 class GroupController:
@@ -10,14 +13,27 @@ class GroupController:
         """
         Handles creating new groups
 
-        :return: result: dict
+        :return: redirect
         """
 
-        groupName = request.form('groupName')
-        groupMembers = request.form('groupMembers')
+        groupName = request.form['groupName']
+        groupMembers = request.form.getlist('groupMembers')
 
         newGroup = Group(groupName=groupName, groupMembers=groupMembers)
         db.session.add(newGroup)
         db.session.commit()
 
         return redirect('/listView')
+
+    @staticmethod
+    def showCreateGroup():
+        """
+        Handles returning the view for creating a group, providing the list of users of the application to the template
+        :return: jinjaTemplate
+        """
+
+        users = User.query.order_by(User.username).all()
+        dictUsers = [JsonService.prepareModel(user) for user in users]
+        jsonUsers = json.dumps(dictUsers)
+        print(jsonUsers)
+        return render_template("create_group.html", users=jsonUsers)
