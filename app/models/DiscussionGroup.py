10ca4181bddcf36ca2a79a345e6491f8bebb5b99
Sajid Kamadia <sarkamadia@mun.ca>
2018-03-21 19:47:47 -0230

    Adding DiscussionGroup database

---
+++ app/models/DiscussionGroup.py
@@ -0,0 +1,28 @@
+from app import db
+
+from datetime import datetime
+
+
+class DiscussionGroup(db.Model):
+
+    """
+        The model mapping to the Discussion Group table
+
+        Attributes:
+            id: int - The primary key ID of the Author
+            title: string - The unique title of what the new post is about
+            body: string - The main information/data of the post.
+            postedby: string - The name of the original author of the post.
+
+        Examples:
+            -To instantiate, use keyword parameters
+                example = Post(title='e', body = 'i am a cat', poster_id = 1)
+    """
+    discussionid = db.Column(db.Integer, primary_key=True)
+    discussiontitle = db.Column(db.String(50), unique=False, nullable=False)
+    discussionbody = db.Column(db.String(255), nullable=False)
+    discussiondateposted = db.Column(db.String(100), nullable=False)
+    discussionposter_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    postedby = db.relationship('User', uselist=False, lazy=False)
+
+
