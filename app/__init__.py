07c99a2c1fe535cfc56e8798372967cc91476e2b
Moustafa Elsisy <spaceflyby@gmail.com>
2018-03-25 17:01:44 -0230

    * Added the web socket and notification service

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+socketio = SocketIO(app)
 
 from app import routes
 from app.modelsImport import *
+
+if __name__ == "__main__":
+    socketio.run(app)
