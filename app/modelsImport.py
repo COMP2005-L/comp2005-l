69bea28fd4615f8c4c744078d8abc8c5a307ea4c
Moustafa Elsisy <spaceflyby@gmail.com>
2018-03-25 20:04:56 -0230

    * Updated GroupController to use the new relationships in the DiscussionGroup model
    * Updated PostController to be able to assign discussion groups to posts, and fire notifications for them.
    
    * Imported missing models into `modelsImport.py`

--- app/modelsImport.py
+++ app/modelsImport.py
@@ -1,2 +1,5 @@
 from app.models.User import User
 from app.models.Post import Post
+from app.models.Notification import Notification
+from app.models.Subscription import Subscription
+from app.models.DiscussionGroup import DiscussionGroup
