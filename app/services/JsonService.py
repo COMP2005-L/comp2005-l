cb81893a237dde69e563b20fdb35e4d1a915ba87
Moustafa Elsisy <spaceflyby@gmail.com>
2018-03-30 08:57:34 -0230

    Added docstrings to services

--- app/services/JsonService.py
+++ app/services/JsonService.py
@@ -1,6 +1,24 @@
 class JsonService:
+    """
+            JsonService takes care of serialization-related functions, such as converting objects to
+            dictionaries, in order to make them serializable.
+
+            Methods:
+                prepareModel
+    """
     @staticmethod
     def prepareModel(instance):
+        """
+        Turns a model instance into a dictionary ready for serialization
+
+        Examples:
+            - To serialize a model instance
+                user = User(...)
+                userDict = JsonService.prepareModel(user)
+
+        :param instance: SQLAlchemy.Model
+        :return: dict
+        """
         serialization = {}
         for c in instance.__table__.columns:
             value = getattr(instance, c.name)
