72a2a9bcb3e494ee4fa0a7f6e542ec70c46fed51
Sajid Kamadia <sarkamadia@mun.ca>
2018-04-01 16:21:32 -0230

    Removing DirectMessaging model because it wasn't reviewed

---
+++ app/models/DirectMessaging.py
@@ -0,0 +1,28 @@
+from app import db
+
+from datetime import datetime
+
+
+
+class DirectMessaging(db.Model):
+    """
+ss
+        The model mapping to the User Direct Messaging table
+
+        Attributes:
+            id: int - The primary key ID of the User sending a message.
+            body: string - The body of the Users' direct message; Body of a post for example
+            sender: string - The id of user sending the message
+            recipient: integer - User id of the recipient of the direct message.
+            date: datetime - The datetime at which the user created/sent the message.
+
+        Examples:
+            -To instantiate, use keyword parameters
+            	example = DirectMessaging(body='text here', sender = 1, recipient=20)
+    
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255), nullable=False)
+    sender = db.relationship(db.String(50),db.ForeignKey("user.id"),nullable=False)
+    recipient = db.Column(db.String(50), db.ForeignKey("user.id"), nullable=False)
+    date = db.Column(db.Datetime, nullable=False)
