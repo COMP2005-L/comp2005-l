9d58f8877d26cbe90b810cfb908175ac9ab2c845
Sajid Kamadia <sarkamadia@mun.ca>
2018-04-01 16:13:01 -0230

    Changing user profile controller

--- app/controllers/userprofile.py
+++ app/controllers/userprofile.py
@@ -8,7 +8,7 @@ from app import db
 class UserProfile:
 
     @staticmethod
-    def showUserProfile(username, postId):
+    def showUserProfile(username):
         """
             Handles the get method for the user to their profile page where they
             can view posts and direct message
@@ -18,28 +18,18 @@ class UserProfile:
         
         if (session.get("logged_in")):
             user = User.query.filter_by(username = username).first()
-            post = Post.query.filter_by(id = postId).first()
-            return render_template("user_Profile.html",user = user, post = post)
+            return render_template("user_Profile.html", user=user)
 
 
     @staticmethod
-    def directMessaging(userId):
+    ## REQUIRES THE SOCKET ASPECT WHICH SAJID AND MOUSTAFA WILL WORK ON
+
+    def directMessaging():
         """
             Handles returning the user to their profile page where they
             can view posts and direct message
-            :param userId: User to send direct message to
-            :return: jinjaTemplate
+
 
         """
-
-        title = request.form["title"]
-        body = request.form["body"]
-        postedBy = request.form["postedBy"]
-
-
-        postedBy = User.query.filter_by(id=session["logged_in"]).first()
-        newPost = Post(title = title, body = body, postedBy = postedBy)
-        db.session.add(newPost)
-        db.session.commit()
 
         return redirect("/userProfile")
