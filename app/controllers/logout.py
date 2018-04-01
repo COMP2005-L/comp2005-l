744b83c5eeec97f4420762a2e0085ad20655283a
Sajid Kamadia <sarkamadia@mun.ca>
2018-04-01 16:59:44 -0230

    Adding logout feature

--- app/controllers/logout.py
+++ app/controllers/logout.py
@@ -9,6 +9,6 @@ class Logout:
         Handles returning the view for logging out, and redirecting to login page
         :return: jinjaTemplate
         """
-        logout_user()
+        session["logged_in"] = -1
         flash("You have successfully logged out!")
         return redirect(url_for('login'))
