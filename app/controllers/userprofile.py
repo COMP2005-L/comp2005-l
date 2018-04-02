94cfb8360987e6703a15773f65edfc223c8a113f
Moustafa Elsisy <spaceflyby@gmail.com>
2018-04-02 00:12:33 -0230

    Added Direct Messaging capability on backend and frontend

--- app/controllers/userprofile.py
+++ app/controllers/userprofile.py
@@ -1,7 +1,8 @@
 from flask import render_template, session, redirect, url_for, request
 from app.models.Post import Post
 from app.models.User import User
-from app.services.NotificationService import NotificationService
+from app.models.DirectMessaging import DirectMessaging
+from app.services.MessagingService import MessagingService
 from app import db
 
 
@@ -18,18 +19,20 @@ class UserProfile:
         
         if (session.get("logged_in")):
             user = User.query.filter_by(username = username).first()
-            return render_template("user_Profile.html", user=user)
+            messages = MessagingService.getConversation(user)
+
+            return render_template("user_Profile.html", user=user, messages=messages)
 
 
     @staticmethod
-    ## REQUIRES THE SOCKET ASPECT WHICH SAJID AND MOUSTAFA WILL WORK ON
-
-    def directMessaging():
+    def directMessaging(recipientId):
         """
-            Handles returning the user to their profile page where they
-            can view posts and direct message
-
-
+            Handles sending a direct message to the targeted user
+            :return: redirect
         """
+        body = request.form.get("body")
+        sender_id = session["logged_in"]
+        dm = DirectMessaging(body=body, sender_id=sender_id, recipient_id=recipientId)
+        MessagingService.send(dm)
 
-        return redirect("/userProfile")
+        return redirect("/userProfile/{}".format(dm.recipient.username))
---
+++ app/services/MessagingService.py
@@ -0,0 +1,47 @@
+from flask import session
+from app import db
+from app.models.Notification import Notification
+from app.models.User import User
+from app.models.DirectMessaging import DirectMessaging
+from app.services.NotificationService import NotificationService
+
+
+class MessagingService:
+
+    @staticmethod
+    def send(directMessage):
+        """
+        Sends a given direct message to the user, by persisting the direct message, and sending
+        a notification to the recipient
+
+        :param directMessage: DirectMessaging - The message to be sent
+        :return: None
+        """
+        db.session.add(directMessage)
+        db.session.commit()
+
+        notification = Notification(title='New Direct Message',
+                                    body='From {}'.format(directMessage.sender.username),
+                                    read=False,
+                                    ref='/userProfile/{}'.format(directMessage.sender.username),
+                                    recipient=directMessage.recipient.id)
+
+        NotificationService.dispatch(notification)
+
+    @staticmethod
+    def getConversation(targetUser):
+        """
+        Retrieves the conversation between the target user and the currently logged in user
+
+        :param targetUser: User
+        :return: list<DirectMessaging>
+        """
+        previewingUser = User.query.filter_by(id=session.get("logged_in")).first()
+        query1 = DirectMessaging.query.filter(DirectMessaging.sender_id == targetUser.id,
+                                              DirectMessaging.recipient_id == previewingUser.id).all()
+        query2 = DirectMessaging.query.filter(DirectMessaging.sender_id == previewingUser.id,
+                                              DirectMessaging.recipient_id == targetUser.id).all()
+        query1.extend(query2)
+        query1.sort(key=lambda message: message.date, reverse=True)
+
+        return query1
