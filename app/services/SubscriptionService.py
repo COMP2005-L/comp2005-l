5979fed4884b9d76ad473ba62974568b3c74342b
Moustafa Elsisy <spaceflyby@gmail.com>
2018-04-02 00:14:15 -0230

    Added Docstrings to SubscriptionService

--- app/services/SubscriptionService.py
+++ app/services/SubscriptionService.py
@@ -6,15 +6,36 @@ class SubscriptionService:
 
     @staticmethod
     def isSubscribed(postId, userId):
+        """
+        Checks if the given user is subscribed to the provided post
+
+        :param postId: int - The id of the post to check subscription against
+        :param userId: int - The id of the user to be checked as a subscriber
+        :return: Subscription|None
+        """
         subscription = Subscription.query.filter(Subscription.post == postId, Subscription.subscriber == userId).first()
         return subscription
 
     @staticmethod
     def getSubscriptions(postId):
+        """
+        Returns a list of all the subscribers for a given post
+
+        :param postId: int - The id of the post to retrieve subscribers for
+        :return: list<Subscription>
+        """
         return Subscription.query.filter_by(post=postId).all()
 
     @staticmethod
     def subscribe(postId, userId):
+        """
+        Subscribe the given user to the provided post, if the user is not already subscribed
+
+        :param postId: int - The id of the post to be subscribed to
+        :param userId: int - The id of the new subscriber
+        :raises ValueError
+        :return: None
+        """
         if (SubscriptionService.isSubscribed(postId, userId)):
             raise ValueError("Already subscribed!")
 
@@ -24,6 +45,14 @@ class SubscriptionService:
 
     @staticmethod
     def unsubscribe(postId, userId):
+        """
+        Unsubscribe the given user from the provided post, if the user is already subscribed
+
+        :param postId: int - The id of the post to be unsubscribed from
+        :param userId: int - The id of the subscriber to be unsubscribed
+        :raises ValueError
+        :return: None
+        """
         if (not SubscriptionService.isSubscribed(postId, userId)):
             raise ValueError("Already unsubscribed!")
 
